e2cc6527d7de5f7d9c0c85354502e7b92f603ca9
Inovax-cito <aaronrisso18@gmail.com>
2024-11-04 01:41:50 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// Selecciona el ícono del carrito
+const carritoIcono = document.getElementById('carrito');
+
+// Añade el evento de click para redirigir
+carritoIcono.addEventListener('click', function(event) {
+    event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+    window.location.href = 'https://Wa.me/541158446409'; // Redirige a la URL deseada
+});
